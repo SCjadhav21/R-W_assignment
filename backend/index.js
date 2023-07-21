c59eae185bd8693d84a32ab7d9f8941e5ee7bd17
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-07-21 09:40:12 +0530

    adding user backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+let express = require("express");
+const { connection } = require("./config/db");
+const cors = require("cors");
+const { UserRoutes } = require("./routes/user.routes");
+
+require("dotenv").config();
+
+let app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", UserRoutes, (req, res) => {
+  res.status(404).send("Routes not found");
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log(`Connected to DataBase`);
+  } catch (err) {
+    console.log("Error: cant connect to mongodb" + err);
+  }
+
+  console.log(`running on port ${process.env.port}`);
+});
---
+++ backend/middelware/authentication.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const Authentication = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  jwt.verify(token, process.env.key, async (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = { Authentication };
